022670f4d6deff9ead66c15b393d7f18a2cb0067
alybenhur <alybenhur@gmail.com>
2022-07-11 09:57:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+from flask_cors import CORS
+from flask import Flask, request
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def anality_page():
+  return "hola mundo"
 
 # 1) load dataset from csv file
 dataset = pd.read_csv('r08iris.csv', delimiter=",")
