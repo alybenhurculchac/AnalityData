cabb267239451b988721aa62c415f1b69f492ac8
alybenhur <alybenhur@gmail.com>
2022-07-11 16:19:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def index():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     clf = MLPClassifier(activation="relu",random_state=1, solver="lbfgs", max_iter=200).fit(X_train, y_train)
     flor = clf.predict([[5.1, 3.8, 1.5, 0.3]])
-    return "Hello Medium"
+    Pre = showPrediction(flor[0])
+    return Pre
 
 
 if __name__ == '__main__':
