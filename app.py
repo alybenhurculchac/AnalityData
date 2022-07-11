594e1508e9587615b933bdc141ea36862e23900b
alybenhur <alybenhur@gmail.com>
2022-07-11 16:08:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def index():
 #dividir el dataset
     X = dataset.iloc[:, 0:4].values
     y = dataset.iloc[:, -1].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    clf = MLPClassifier(activation="relu",random_state=1, solver="lbfgs", max_iter=200).fit(X_train, y_train)
+    flor = clf.predict([[5.1, 3.8, 1.5, 0.3]])
     return "Hello Medium"
 
 
