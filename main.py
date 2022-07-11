1ba3ef4951096d576dd50a5fa4456c73b6c9bc4b
alybenhur <alybenhur@gmail.com>
2022-07-11 10:43:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from flask import Flask, request
 app = Flask(__name__)
 CORS(app)
 @app.route('/')
-def ontology_page():
-   return 
-
+def homepage():
+    return """
+    <h1>Hello heroku</h1>
+  
 port=os.environ["PORT"]
 app.run('0.0.0.0',port, debug=True)
 
