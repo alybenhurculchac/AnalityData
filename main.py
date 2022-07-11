b165626a1eab1e331eb0c3aa521b2ce4e9ed2511
alybenhur <alybenhur@gmail.com>
2022-07-11 10:14:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import pandas as pd
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
 from flask_cors import CORS
 from flask import Flask, request
 
