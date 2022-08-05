1f9679f49508b57f00a35646adacf2388171e4e3
alybenhur <alybenhur@gmail.com>
2022-08-05 10:31:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 
@@ -22,10 +23,11 @@ def index():
     y = dataset.iloc[:, -1].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     clf = MLPClassifier(activation="relu",random_state=1, solver="lbfgs", max_iter=200).fit(X_train, y_train)
-    flor = clf.predict([[5.1, 3.8, 1.5, 0.3]])
-    Pre = showPrediction(flor[0])
-    y_pred = clf.predict(X_test)
-    html = """<p>>%s</p>""" % y_pred
+   
+    # flor = clf.predict([[5.1, 3.8, 1.5, 0.3]])
+    # Pre = showPrediction(flor[0])
+    #y_pred = clf.predict(X_test)
+    #html = """<p>>%s</p>""" % y_pred
     return html
 
 
